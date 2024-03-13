6eac0240cd1261816e7dd6a35c207d1271a2eaed
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2024-03-13 16:36:57 +0900

    init

--- settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "_22_fairy"
-
---
+++ src/test/java/fairy/spring/fairy/FairyApplicationTests.java
@@ -0,0 +1,13 @@
+package fairy.spring.fairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
