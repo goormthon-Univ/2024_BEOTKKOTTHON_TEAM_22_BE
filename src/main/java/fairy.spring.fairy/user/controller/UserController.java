2dc3d1015b05a6eb750a310516cd64d3bf32138b
chaeheun <alzl7410@gmail.com>
2024-03-15 00:12:42 +0900

    스웨거 적용

--- src/main/java/fairy.spring.fairy/user/controller/UserController.java
+++ src/main/java/fairy.spring.fairy/user/controller/UserController.java
@@ -6,6 +6,11 @@ import fairy.spring.fairy.config.utils.ApiResponseBuilder;
 import fairy.spring.fairy.user.request.UserRequest;
 import fairy.spring.fairy.user.response.UserResponse;
 import fairy.spring.fairy.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,19 +25,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 //@Tag(name = "User", description = "유저 API")
+@Tag(name = "User", description = "유저 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
 
-
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
+    @ApiResponse(responseCode = "200", description = "회원가입 성공")
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid UserRequest.SignupRequestDTO requestDTO, Errors errors) {
         userService.signup(requestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.successWithNoContent());
     }
-
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+    @ApiResponse(responseCode = "200", description = "로그인 성공",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponse.LoginResponseDTO.class)))
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginRequestDTO requestDTO, Errors errors, HttpServletResponse response) {
         //TO-DO : 로그인 로직 구현하기
