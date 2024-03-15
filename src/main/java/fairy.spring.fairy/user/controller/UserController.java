8a3deabb94402e77e5c65f6d21192e1f61acb8a0
chaeheun <alzl7410@gmail.com>
2024-03-15 15:21:21 +0900

    스웨거 적용

--- src/main/java/fairy.spring.fairy/user/controller/UserController.java
+++ src/main/java/fairy.spring.fairy/user/controller/UserController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//@Tag(name = "User", description = "유저 API")
+
 @Tag(name = "User", description = "유저 API")
 @RestController
 @RequiredArgsConstructor
