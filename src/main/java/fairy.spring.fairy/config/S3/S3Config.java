b080477839af8277dffd430731d32256a21c38d5
Teddy (Dongho) <gongdongho12@gmail.com>
2024-03-20 00:50:59 +0900

    chore: s3 config 파일명 정정

--- src/main/java/fairy.spring.fairy/config/S3/S3Config.java
+++ src/main/java/fairy.spring.fairy/config/S3/S3Config.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class S3Config2 {
+public class S3Config {
 
     @Value("${AWS_ACCESS_KEY_ID}")
     private String accessKey;
