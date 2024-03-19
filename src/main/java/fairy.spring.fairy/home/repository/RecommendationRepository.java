cab6cb7aa3c7dc23db58d57121ad76d24690c252
Teddy (Dongho) <gongdongho12@gmail.com>
2024-03-20 01:39:46 +0900

    feat: web 8080 test

--- src/main/java/fairy.spring.fairy/home/repository/RecommendationRepository.java
+++
@@ -1,7 +0,0 @@
-package fairy.spring.fairy.home.repository;
-
-import fairy.spring.fairy.home.entity.Recommendation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-}
--- src/test/java/fairy/spring/fairy/FairyApplicationTests.java
+++ src/test/java/fairy/spring/fairy/FairyApplicationTests.java
@@ -1,9 +1,5 @@
 package fairy.spring.fairy;
 
-import fairy.spring.fairy.Community.Request.CommunityRequest;
-import fairy.spring.fairy.Community.controller.Questioncontroller;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
