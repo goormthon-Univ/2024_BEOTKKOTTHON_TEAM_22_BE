c8d8bea5cbaeafca9a4114bd591002f32203e388
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2024-03-24 04:55:00 +0900

    tip

--- src/main/java/fairy.spring.fairy/home/entity/Tip.java
+++ src/main/java/fairy.spring.fairy/home/entity/Tip.java
@@ -1,16 +1,13 @@
 package fairy.spring.fairy.home.entity;
-import jakarta.persistence.*;
-//import fairy.spring.fairy.home.entity.Recommendation;
 
-//import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-//import lombok.Setter;
+
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
+@Setter
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Tip {
@@ -25,18 +22,27 @@ public class Tip {
     private Category category;
 
     private String content;
-
+    private boolean bookmarkStatus; // 북마크 상태를 나타내는 필드
+    private int bookmarkCounts; // 북마크 수를 나타내는 필드
 
     @OneToMany(mappedBy = "tip")
     private List<Recommendation> recommendations;
+    private String imageUrl;
+     @Builder
+    // 생성자를 통해 객체를 초기화할 수 있도록 생성자를 추가
+    public Tip(String title, Category category, String content, long id, long categoryType, int bookmarkCounts, boolean bookmarkStatus, String imageUrl) {
 
-    public int getBookmarkCounts(){
+         this.title = title;
+        this.category = category;
+        this.content = content;
+        this.id = id;
+        this.bookmarkCounts = bookmarkCounts;
+        this.bookmarkStatus = bookmarkStatus;
 
-        return recommendations.size();
     }
 
-    public boolean getBookmarkStatus(){
+    // 필요한 setter 메서드들은 이미 Lombok을 통해 자동 생성됨
 
-        return recommendations.size() > 0;
-    }
+    // BookmarkStatus를 설정하는 setter 메서드 추가
+
 }
--- src/main/java/fairy.spring.fairy/home/service/TipService.java
+++ src/main/java/fairy.spring.fairy/home/service/TipService.java
@@ -1,6 +1,8 @@
 package fairy.spring.fairy.home.service;
 
+
 import fairy.spring.fairy.home.entity.Category;
+import fairy.spring.fairy.home.entity.CategoryType;
 import fairy.spring.fairy.home.entity.Tip;
 
 import java.util.List;
@@ -9,5 +11,8 @@ public interface TipService {
 
     List<Tip> getAllTips();
     List<Tip> getTipsByCategory(Category category);
+
+    List<Tip> getTipsByCategory(CategoryType category);
+
     List<Tip> searchTipsByKeyword(String keyword);
 }
